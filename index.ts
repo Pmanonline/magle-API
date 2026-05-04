6eccca1ec4d28b5498a76fab8e78e67ad604ae4f
Pmanonline <petersonzoconis@gmail.com>
2026-05-04 19:21:55 +0100

    pushing cors changes

--- index.ts
+++ index.ts
@@ -18,9 +18,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin:
+      process.env.FRONTEND_URL ||
+      "http://localhost:5173" ||
+      "https://magle-frontend.vercel.app",
     credentials: true,
-  })
+  }),
 );
 app.use(cookieParser());
 app.use(express.json());
